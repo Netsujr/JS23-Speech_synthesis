44ffa08b1b817ddda43bda15331404e0891cb717
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-04 09:59:36 +0900

    added voices to dropdown

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const msg = new SpeechSynthesisUtterance();
+const msg = new SpeechSynthesisUtterance(); //the api
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+// create the voices array
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+  .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+  .join('');
+
+}
